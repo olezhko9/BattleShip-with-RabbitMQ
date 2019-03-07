85437e8af8917985205aae5137acdf41dfd9f8be
Oleg Naumov <oleg.naumov.98@mail.ru>
2019-03-07 23:42:01 +0300

    Do some refactoring, add comments

--- BattleField.py
+++ BattleField.py
@@ -14,8 +14,7 @@ class BattleField(QWidget):
     MISS_CELL = 4
     FREE_CELL = 5
 
-    FIELD_SIZE = 10
-
+    FIELDS_NUM = 10
 
     def __init__(self, enemy_field=False):
         super(BattleField, self).__init__()
@@ -41,6 +40,7 @@ class BattleField(QWidget):
             for c in range(10):
                 self.table.setItem(r, c, QTableWidgetItem())
 
+        self.table.setEditTriggers(QTableWidget.NoEditTriggers)
         self.table.setSelectionMode(QAbstractItemView.NoSelection)
         self.table.cellClicked.connect(self.cell_clicked)
 
@@ -54,50 +54,48 @@ class BattleField(QWidget):
         """
         Случайная расстановка кораблей и их отрисовка
         """
-        self.fleet = [
-            [0,0,0,0,0,0,0,0,0,0],
-            [0,0,0,0,0,0,0,0,0,0],
-            [0,0,0,0,0,0,0,0,0,0],
-            [0,0,0,0,0,0,0,0,0,0],
-            [0,0,0,0,0,0,0,0,0,0],
-            [0,0,0,0,0,0,0,0,0,0],
-            [0,0,0,0,0,0,0,0,0,0],
-            [0,0,0,0,0,0,0,0,0,0],
-            [0,0,0,0,0,0,0,0,0,0],
-            [0,0,0,0,0,0,0,0,0,0],
-        ]
+        # Изначально поле битвы пустое
+        self.fleet = [[0] * self.FIELDS_NUM for i in range(self.FIELDS_NUM)]
 
+        # Флот кораблей. 1 четырехпалубный, 2 трехпалубных, ...
         ship_fleet = [(1, 4), (2, 3), (3, 2), (4, 1)]
 
+        # записываем корабли в матрицу расположения флота
         for ship in ship_fleet:
             for ship_count in range(ship[0]):
                 valid = False
                 random_x, random_y = -1, -1
                 o = choice([Ship.H_ORIENTATION, Ship.V_ORIENTATION])
                 while not valid:
-                    random_x = randint(0, 9)
-                    random_y = randint(0, 9)
+                    random_x = randint(0, self.FIELDS_NUM - 1)
+                    random_y = randint(0, self.FIELDS_NUM - 1)
                     valid = self.is_valid_position(random_x, random_y, o, ship[1])
                 self.place_ship(random_x, random_y, o, ship[1])
 
+        # отрисовываем корабли, если они находятся на нашем поле
         if not self.enemy_field:
             for y, line in enumerate(self.fleet):
                 for x, cell in enumerate(line):
-                    if cell == 1:
+                    if cell == self.SHIP_CELL:
                         self.table.item(y, x).setBackground(QColor(100, 100, 150))
 
     def is_valid_position(self, start_x, start_y, orientation, length):
+        """
+        Проверка указанных координат и ориентации кораблся на пересечение с другими кораблями и
+        на выход за границы игрового поля.
+        :return: True, если корабль можно поставить в указанную позицию, иначе False.
+        """
         end_x, end_y = -1, -1
         if orientation == Ship.H_ORIENTATION:
-            if start_x + length > self.FIELD_SIZE:
+            if start_x + length > self.FIELDS_NUM:
                 return False
-            end_x = min(start_x + length, 10 - 1)
-            end_y = min(start_y + 1, 10 - 1)
+            end_x = min(start_x + length, self.FIELDS_NUM - 1)
+            end_y = min(start_y + 1, self.FIELDS_NUM - 1)
         elif orientation == Ship.V_ORIENTATION:
-            if start_y + length > self.FIELD_SIZE:
+            if start_y + length > self.FIELDS_NUM:
                 return False
-            end_x = min(start_x + 1, 10 - 1)
-            end_y = min(start_y + length, 10 - 1)
+            end_x = min(start_x + 1, self.FIELDS_NUM - 1)
+            end_y = min(start_y + length, self.FIELDS_NUM - 1)
 
         start_x = start_x if start_x == 0 else start_x - 1
         start_y = start_y if start_y == 0 else start_y - 1
@@ -109,6 +107,10 @@ class BattleField(QWidget):
         return True
 
     def place_ship(self, start_x, start_y, orientation, length):
+        """
+        Ставит корабль в начальную точку и располагает его согласно значению ориентации.
+        Считается, что задаваемое положение проверено с помощью функции is_valid_position.
+        """
         end_x, end_y = -1, -1
         if orientation == Ship.H_ORIENTATION:
             end_x = start_x + length
@@ -117,6 +119,7 @@ class BattleField(QWidget):
             end_x = start_x + 1
             end_y = start_y + length
 
+        # сохраняем информацию о корабле в матрицу расположения флота
         for x in range(start_x, end_x):
             for y in range(start_y, end_y):
                 self.fleet[x][y] = self.SHIP_CELL
@@ -125,12 +128,18 @@ class BattleField(QWidget):
     @pyqtSlot()
     def cell_clicked(self):
         """
-        Вызывается при нажатии на клетку игрового поля
+        Вызывается при нажатии на клетку игрового поля. Если клик на вражеском поле, то обрабатываем.
         """
+        item = self.table.currentItem()
         if self.enemy_field:
             print("enemy")
+            row = item.row()
+            col = item.column()
+            if self.fleet[row][col] == self.SHIP_CELL:
+                item.setBackground(QColor(150, 100, 150))
+            elif self.fleet[row][col] == self.EMPTY_CELL:
+                item.setBackground(QColor(170, 255, 255))
+            print(item.row(), item.column())
         else:
             print("me")
-        item = self.table.currentItem()
-        print(item.row(), item.column())
 
